30a5c52ec9d34dcb7c1a60aefe060ddecf5a0c5c
lisajoanno <lisajoanno@live.fr>
2015-01-20 11:34:45 +0100

    suppressions aussi

---
+++ src/zuul/place/Corridor.java
@@ -0,0 +1,144 @@
+package zuul.place;
+
+import zuul.Game;
+import zuul.foundobject.Cheatsheet;
+import zuul.foundobject.Tablet;
+import zuul.person.Student;
+
+import java.util.Random;
+
+/**
+ * @author Mathieu Molinengo
+ * @version 2014.11.30
+ * 
+ *          Class to create a Corridor which gives access to other room
+ */
+public class Corridor extends Room {
+	private boolean lights, tablet, cheatsheet;
+	private Tablet tabletInTheCorridor;
+	private Cheatsheet cheatsheetInTheCorridor;
+
+	/***
+	 * 
+	 * Constructor to create a Corridor
+	 * @param description the description of the corridor
+	 * 
+	 ***/
+	public Corridor(String description) {
+		super(description);
+		Random randomLight = new Random();
+		randomLight.nextBoolean();
+
+		lights = (randomLight.nextBoolean()) ? true : false;
+
+	}
+
+	/***
+	 * 
+	 * Method to randomize the apparition of tablet and cheatsheet
+	 * 
+	 ***/
+	public void appearObject() {
+		int chance1 = (int) (Math.random() * 20);
+		tablet = (chance1 > 14) ? true : false;
+		int chance2 = (int) (Math.random() * 20);
+		cheatsheet = (chance2 > 17) ? true : false;
+
+	}
+
+	/***
+	 * 
+	 * Method to know if the student can enter in this corridor
+	 * @param student the student who wants to go in
+	 * @return true
+	 * 
+	 ***/
+	@Override
+	public boolean canEnter(Student student) {
+		return true;
+	}
+
+	/***
+	 * 
+	 * Method to enter in this corridor
+	 * @param student the student who wants to go in
+	 * @return true
+	 * 
+	 ***/
+	@Override
+	public boolean enter(Student student) {
+		if (isLights()) {
+			appearObject();
+			if (tablet) {
+				tabletInTheCorridor = new Tablet();
+				System.out.println(Game.res.getString("corridor.tablet"));
+			}
+			if (cheatsheet) {
+				cheatsheetInTheCorridor = new Cheatsheet();
+				System.out.println(Game.res.getString("corridor.cheatsheet"));
+			}
+			System.out.println(getLongDescription());
+		} else {
+			System.out.println(Game.res.getString("corridor.dark"));
+			System.out.println(getExitString());
+		}
+
+		return true;
+	}
+
+	/***
+	 * 
+	 * Method to use tablet in this corridor
+	 * @param student the student who wants to use it
+	 * 
+	 ***/
+	public void useTablet(Student student) {
+		if(tablet){
+			tabletInTheCorridor.useObject(student);
+			tablet = false;
+		} else {
+			System.out.println(Game.res.getString("corridor.notablet"));
+		}
+		System.out.println(this.getLongDescription());
+	}
+
+	/***
+	 * 
+	 * Method to use cheatsheet in this corridor
+	 * @param student the student who wants to use it
+	 * 
+	 ***/
+	public void useCheatsheet(Student student) {
+		if(cheatsheet){
+			cheatsheetInTheCorridor.useObject(student);
+			cheatsheet = false;
+		} else{
+			System.out.println(Game.res.getString("corridor.nocheatsheet"));
+		}
+		System.out.println(this.getLongDescription());
+	}
+
+	/***
+	 * 
+	 * Method to change the boolean lights to true
+	 * 
+	 */
+	public void turnLightsOn() {
+		lights = true;
+	}
+
+	/**
+	 * @return the lights
+	 */
+	public boolean isLights() {
+		return lights;
+	}
+
+	/**
+	 * @param lights
+	 *            the lights to set
+	 */
+	public void setLights(boolean lights) {
+		this.lights = lights;
+	}
+}
---
+++ src/zuul/place/Library.java
@@ -0,0 +1,125 @@
+package zuul.place;
+
+import java.util.ArrayList;
+
+import zuul.Game;
+import zuul.course.LectureItem;
+import zuul.person.Student;
+
+/**
+ * @author Lucas Martinez and Molinengo Mathieu
+ * @version 2014.11.30
+ * 
+ *          Class to create a LabRoom
+ */
+public class Library extends Room {
+
+	boolean isOpen = false;
+	private ArrayList<LectureItem> poo2Book;
+
+	/***
+	 * 
+	 * Constructor to create a Library
+	 * 
+	 * @param description
+	 *            the description of the library
+	 * 
+	 ***/
+	public Library(String description) {
+		super(description);
+		poo2Book = new ArrayList<LectureItem>();
+
+		LectureItem poo2BookPage1 = new LectureItem("OOP", 1, "oop.lecture1");
+		LectureItem poo2BookPage2 = new LectureItem("OOP", 2, "oop.lecture2");
+		LectureItem poo2BookPage3 = new LectureItem("OOP", 3, "oop.lecture3");
+		poo2Book.add(poo2BookPage1);
+		poo2Book.add(poo2BookPage2);
+		poo2Book.add(poo2BookPage3);
+
+	}
+
+	/***
+	 * 
+	 * This function determines if the student can enter the library or not
+	 * 
+	 * @param student
+	 *            who wants enter in the labroom
+	 * @return true if the student can enter and false if he can't
+	 * 
+	 */
+	public boolean canEnter(Student student) {
+		return isOpen();
+	}
+
+	/***
+	 * 
+	 * Method to enter in this library
+	 * 
+	 * @param student
+	 *            the student who wants to go in
+	 * @return true if he can go in and false if he can't
+	 * 
+	 ***/
+	public boolean enter(Student student) {
+		if (canEnter(student)) {
+			System.out.println(Game.res.getString("library.description") + "\n"
+					+ getExitString());
+
+			return true;
+		}
+		System.out.println(Game.res.getString("library.closed"));
+		return false;
+	}
+
+	/***
+	 * Method to randomize the opening of the library
+	 * 
+	 * @return true if the library is open or not if it isn't
+	 */
+	public boolean isOpen() {
+		int random = (int) (Math.random() * 10);
+
+		isOpen = random > 7;
+		return isOpen;
+	}
+
+	/***
+	 * 
+	 * Method to set the poo2Book in the Student to can use it later
+	 * 
+	 * @param student
+	 *            the student who wants take the book
+	 */
+	public void takeBook(Student student) {
+		student.setOOPbook(poo2Book);
+		System.out.println(Game.res.getString("library.takebook") + "\n"
+				+ getExitString());
+	}
+
+	/***
+	 * 
+	 * Method to read the poo2Book without take it
+	 * 
+	 * @param goodStudent
+	 *            the Student who wants learn all the OOP lectures
+	 * 
+	 */
+	public void learnPOO(Student goodStudent) {
+		int sizeBook = poo2Book.size();
+		for (int i = 0; i < sizeBook; i++) {
+			if (!goodStudent.alreadyListenedLecture(poo2Book.get(i))) {
+				goodStudent.addItem(poo2Book.get(i));
+				System.out.println(Game.res.getString(poo2Book.get(i)
+						.getBundleKey()) + "\n");
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		goodStudent.decrementEnergy(35);
+		System.out.println(getExitString());
+
+	}
+}
