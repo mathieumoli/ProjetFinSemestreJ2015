34faef1dcabe0f4d9acb21c0e62d9df4f5c393d6
Mathieu <mathieu.moli@hotmail.com>
2015-01-20 15:26:16 +0100

    Room & ExamRoom

--- src/zuul/place/ExamRoom.java
+++ src/zuul/place/ExamRoom.java
@@ -1,6 +1,7 @@
 package zuul.place;
 
 import zuul.Game;
+import zuul.commands.Command;
 import zuul.course.LabItem;
 import zuul.course.LectureItem;
 import zuul.person.Student;
@@ -24,7 +25,9 @@ public class ExamRoom extends Room {
 	/***
 	 * 
 	 * Constructor to create an ExamRoom
-	 * @param description the description of the examroom
+	 * 
+	 * @param description
+	 *            the description of the examroom
 	 * 
 	 ***/
 	public ExamRoom(String description) {
@@ -76,11 +79,11 @@ public class ExamRoom extends Room {
 			labOOP.add(new LabItem("OOP", i + 1));
 		}
 		allCourses = ((student.getCoursSuivi().contains(lectureOOP.get(0)))
-				&& (student.getCoursSuivi().contains(lectureOOP.get(1))) && (student
-				.getCoursSuivi().contains(lectureOOP.get(2))));
+		        && (student.getCoursSuivi().contains(lectureOOP.get(1))) && (student
+		        .getCoursSuivi().contains(lectureOOP.get(2))));
 		allLabs = ((student.getLabsSuivi().contains(labOOP.get(0)))
-				&& (student.getLabsSuivi().contains(labOOP.get(1))) && (student
-				.getLabsSuivi().contains(labOOP.get(2))));
+		        && (student.getLabsSuivi().contains(labOOP.get(1))) && (student
+		        .getLabsSuivi().contains(labOOP.get(2))));
 
 		return (allCourses && allLabs);
 	}
@@ -88,7 +91,9 @@ public class ExamRoom extends Room {
 	/***
 	 * 
 	 * Method to enter in this examroom
-	 * @param student the student who wants to go in
+	 * 
+	 * @param student
+	 *            the student who wants to go in
 	 * @return true if he can go in and false if he can't
 	 * 
 	 ***/
@@ -98,21 +103,21 @@ public class ExamRoom extends Room {
 		if (canEnter(student)) {
 			if (exam.equals("noexam")) {
 				System.out.println(Game.res
-						.getString("examroom.shortdescription")
-						+ "\n"
-						+ Game.res.getString("examroom.noexam")
-						+ "\n"
-						+ getExitString());
+				        .getString("examroom.shortdescription")
+				        + "\n"
+				        + Game.res.getString("examroom.noexam")
+				        + "\n"
+				        + getExitString());
 			} else {
 				System.out.println(Game.res.getString("examroom.description"));
 				if (exam.equals("OOP")) {
 					System.out.println(Game.res
-							.getString("OOP.exam.description"));
+					        .getString("OOP.exam.description"));
 					startExam(student);
 
 				} else {
 					System.out.println(Game.res.getString(exam
-							+ ".exam.description"));
+					        + ".exam.description"));
 					System.out.println(getExitString());
 				}
 			}
@@ -122,7 +127,7 @@ public class ExamRoom extends Room {
 		}
 		return true;
 	}
-	
+
 	/***
 	 * 
 	 * Method to randomize the exam in the examroom (it will have no exam too)
@@ -142,7 +147,9 @@ public class ExamRoom extends Room {
 	/***
 	 * 
 	 * Method to start an exam
-	 * @param student the student who passes it
+	 * 
+	 * @param student
+	 *            the student who passes it
 	 * 
 	 ***/
 	public void startExam(Student student) {
@@ -154,22 +161,22 @@ public class ExamRoom extends Room {
 
 			for (int i = 1; i <= NB_QUESTIONS; ++i) {
 				System.out.println(Game.res.getString(exam + ".exam.question"
-						+ i));
+				        + i));
 				answer = scanner.nextLine();
 				String rightAnswer = Game.res.getString(exam + ".exam.answer"
-						+ i);
+				        + i);
 				if (answer.toUpperCase().equals(rightAnswer)) {
 					++questionsRight;
 				}
 
 				System.out.println(Game.res.getString("exam.rightanswer")
-						+ rightAnswer);
+				        + rightAnswer);
 				System.out.println();
 			}
 
 			System.out.println(Game.res.getString("student.youhave")
-					+ questionsRight + "/" + NB_QUESTIONS
-					+ Game.res.getString("student.answers"));
+			        + questionsRight + "/" + NB_QUESTIONS
+			        + Game.res.getString("student.answers"));
 			if (questionsRight > (NB_QUESTIONS / 2)) {
 				System.out.println(Game.res.getString("game.win") + exam + ".");
 				student.decrementEnergy(40);
@@ -179,7 +186,7 @@ public class ExamRoom extends Room {
 				}
 			} else {
 				System.out.println(Game.res.getString("game.lose") + exam
-						+ Game.res.getString("game.lose2"));
+				        + Game.res.getString("game.lose2"));
 				student.decrementEnergy(40);
 				System.out.println(getExitString());
 			}
@@ -199,5 +206,31 @@ public class ExamRoom extends Room {
 	 */
 	public void setExam(String exam) {
 		this.exam = exam;
+	}
+
+	/**
+	 * Try to start the exam, if it's incomplete, display an error message
+	 * 
+	 * @param command
+	 *            The command to be processed.
+	 */
+	@Override
+	public void wantStart(Command command, Student gamer) {
+		if (!command.hasSecondWord()) {
+			// if there is no second word, we don't know where to go...
+			System.out.println(Game.res.getString("game.start"));
+		} else if (command.getSecondWord().equals("exam")){
+			this.startExam(gamer);
+			if (this.getExam().equals("noexam")) {
+				System.out.println(Game.res
+				        .getString("examroom.shortdescription")
+				        + "\n"
+				        + Game.res.getString("examroom.noexam")
+				        + "\n"
+				        + this.getExitString());
+			} else
+				System.out.println(this.getLongDescription());
+
+		}
 	}
 }
--- src/zuul/place/Room.java
+++ src/zuul/place/Room.java
@@ -1,6 +1,9 @@
 package zuul.place;
 
+
 import zuul.Game;
+import zuul.commands.Command;
+import zuul.commands.Parser;
 import zuul.person.Student;
 
 import java.util.ResourceBundle;
@@ -10,120 +13,191 @@ import java.util.HashMap;
 /**
  * Class Room - a room in an adventure game.
  *
- * This class is part of the "World of Zuul" application. 
- * "World of Zuul" is a very simple, text based adventure game.  
+ * This class is part of the "World of Zuul" application. "World of Zuul" is a
+ * very simple, text based adventure game.
  *
- * A "Room" represents one location in the scenery of the game.  It is 
- * connected to other rooms via exits.  For each existing exit, the room 
- * stores a reference to the neighboring room.
+ * A "Room" represents one location in the scenery of the game. It is connected
+ * to other rooms via exits. For each existing exit, the room stores a reference
+ * to the neighboring room.
  * 
- * @author  Michael Kölling and David J. Barnes
+ * @author Michael Kölling and David J. Barnes
  * @version 2011.08.10
  */
 
-public abstract class Room
-{
-    protected String description;
-    protected boolean isSecret;
-    protected HashMap<String, Room> exits;        // stores exits of this room.
+public abstract class Room {
+	protected String description;
+	protected boolean isSecret;
+	protected HashMap<String, Room> exits; // stores exits of this room.
 
-    /**
-     * Abstract method which will be overriden in each class
-     * @param student
-     * @return
-     */
-    public abstract boolean enter(Student student);
+	/**
+	 * Abstract method which will be overriden in each class
+	 * 
+	 * @param student
+	 * @return
+	 */
+	public abstract boolean enter(Student student);
 
-    /**
-     * Abstract method which will determine if the student can enter
-     * @param student
-     * @return
-     */
-    public abstract boolean canEnter(Student student);
+	/**
+	 * Abstract method which will deterimport java.util.*;mine if the student can enter
+	 * 
+	 * @param student
+	 * @return
+	 */
+	public abstract boolean canEnter(Student student);
 
+	/**
+	 * Create a room described "description". Initially, it has no exits.
+	 * "description" is something like "a kitchen" or "an open court yard".
+	 * 
+	 * @param description
+	 *            The room's description.
+	 */
+	public Room(String description) {
+		this.description = description;
+		exits = new HashMap<String, Room>();
+		isSecret = false;
+	}
 
-    /**
-     * Create a room described "description". Initially, it has
-     * no exits. "description" is something like "a kitchen" or
-     * "an open court yard".
-     * @param description The room's description.
-     */
-    public Room(String description) 
-    {
-        this.description = description;
-        exits = new HashMap<String, Room>();
-        isSecret=false;
-    }
-    
-    /**
-     * Create a room described "description". Initially, it has
-     * no exits. "description" is something like "a kitchen" or
-     * "an open court yard".
-     * @param description The room's description.
-     * @boolean secret if the room is a secret room or not
-     */
-    public Room(String description,boolean secret) 
-    {
-        this.description = description;
-        exits = new HashMap<String, Room>();
-        isSecret=secret;
-    }
+	/**
+	 * Create a room described "description". Initially, it has no exits.
+	 * "description" is something like "a kitchen" or "an open court yard".
+	 * 
+	 * @param description
+	 *            The room's description.
+	 * @boolean secret if the room is a secret room or not
+	 */
+	public Room(String description, boolean secret) {
+		this.description = description;
+		exits = new HashMap<String, Room>();
+		isSecret = secret;
+	}
+	
+	/**
+	 * Create a room described "description". Initially, it has no exits.
+	 * "description" is something like "a kitchen" or "an open court yard".
+	 * 
+	 * @param description
+	 *            The room's description.
+	 * @boolean secret if the room is a secret room or not
+	 */
+	public Room() {
+		this.description = null;
+		exits = new HashMap<String, Room>();
+		isSecret = false;
+	}
 
-    /**
-     * Define an exit from this room.
-     * @param direction The direction of the exit.
-     * @param neighbor  The room to which the exit leads.
-     */
-    public void setExit(String direction, Room neighbor) 
-    {
-        exits.put(direction, neighbor);
-    }
+	/**
+	 * Define an exit from this room.
+	 * 
+	 * @param direction
+	 *            The direction of the exit.
+	 * @param neighbor
+	 *            The room to which the exit leads.
+	 */
+	public void setExit(String direction, Room neighbor) {
+		exits.put(direction, neighbor);
+	}
 
-    /**
-     * @return The short description of the room
-     * (the one that was defined in the constructor).
-     */
-    public String getShortDescription()
-    {
-        return description;
-    }
+	/**
+	 * @return The short description of the room (the one that was defined in
+	 *         the constructor).
+	 */
+	public String getShortDescription() {
+		return description;
+	}
 
-    /**
-     * Return a description of the room in the form:
-     *     You are in the kitchen.
-     *     Exits: north west
-     * @return A long description of this room
-     */
-    public String getLongDescription()
-    {
-        return description + ".\n" + getExitString();
-    }
+	/**
+	 * Return a description of the room in the form: You are in the kitchen.
+	 * Exits: north west
+	 * 
+	 * @return A long description of this room
+	 */
+	public String getLongDescription() {
+		return description + ".\n" + getExitString();
+	}
 
-    /**
-     * Return a string describing the room's exits, for example
-     * "Exits: north west".
-     * @return Details of the room's exits.
-     */
-    public String getExitString()
-    {
-        String returnString = Game.res.getString("game.exits");
-        Set<String> keys = exits.keySet();
-        for(String exit : keys) {
-            returnString += " " + exit;
-        }
-        return returnString;
-    }
+	/**
+	 * Return a string describing the room's exits, for example
+	 * "Exits: north west".
+	 * 
+	 * @return Details of the room's exits.
+	 */
+	public String getExitString() {
+		String returnString = Game.res.getString("game.exits");
+		Set<String> keys = exits.keySet();
+		for (String exit : keys) {
+			returnString += " " + exit;
+		}
+		return returnString;
+	}
 
-    /**
-     * Return the room that is reached if we go from this room in direction
-     * "direction". If there is no room in that direction, return null.
-     * @param direction The exit's direction.
-     * @return The room in the given direction.
-     */
-    public Room getExit(String direction) 
-    {
-        return exits.get(direction);
-    }
+	/**
+	 * Return the room that is reached if we go from this room in direction
+	 * "direction". If there is no room in that direction, return null.
+	 * 
+	 * @param direction
+	 *            The exit's direction.
+	 * @return The room in the given direction.
+	 */
+	public Room getExit(String direction) {
+		return exits.get(direction);
+	}
+
+	/**
+	 * Try to start the exam, if it's incomplete, display an error message
+	 * 
+	 * @param command
+	 *            The command to be processed.
+	 */
+	private void wantStart(Command command) {
+		// TODO internationalisation
+		System.out.println("You can't do this here !");
+	}
+
+	// TODO
+	public void displayHelp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * To deal with starting command
+	 * 
+	 * @param command
+	 *            the command which must be deal
+	 * @param gamer
+	 *            the student who want to start something
+	 */
+	public void wantStart(Command command, Student gamer) {
+		// TODO internationalisation
+		System.out.println("You can't do this here");
+	}
+
+	/**
+	 * @return the exits
+	 */
+	public HashMap<String, Room> getExits() {
+		return exits;
+	}
+
+	/**
+	 * @param exits
+	 *            the exits to set
+	 */
+	public void setExits(HashMap<String, Room> exits) {
+		this.exits = exits;
+	}
+
+	/**
+	 * Print out some help information. Here we print some stupid, cryptic
+	 * message and a list of the command words.
+	 */
+	public void printHelp(Parser parser) {
+		System.out.println(Game.res.getString("game.help2"));
+		System.out.println(Game.res.getString("game.help3"));
+		System.out.println();
+		System.out.println(Game.res.getString("game.help4"));
+		parser.showCommands();
+	}
 
 }
-
