d876cdec11f118f954d51f8536af0d584d317e1f
Mathieu <mathieu.moli@hotmail.com>
2015-01-20 09:49:28 +0100

    First Commit Maraudeur Map

---
+++ src/zuul/foundobject/CarteMaraudeur.java
@@ -0,0 +1,16 @@
+package zuul.foundobject;
+/**
+ * 
+ * @author Mathieu Molinengo
+ *
+ */
+public class CarteMaraudeur {
+	
+	private ArrayList<Person> persons=new ArrayList<Person>();
+	
+	
+
+	public CarteMaraudeur(){
+		Arraylist
+	}
+}
