0100ad0bf0050cf27bf8af45b57b9ad7a76ce0c2
cedric <cedricl.fr@gmail.com>
2015-01-22 09:34:59 +0100

    Remove the scanner.close() in maroudeur

--- src/zuul/object/MaraudeurMap.java
+++ src/zuul/object/MaraudeurMap.java
@@ -95,7 +95,6 @@ public class MaraudeurMap extends ZuulObject{
 			Scanner scanner = new Scanner(System.in);
 			String pass = scanner.nextLine();
 			unlockMap(pass);
-			scanner.close();
 		}
 
 	}
